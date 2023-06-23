838c5a6e096660ac5216f76a1afcab161ac767b6
manlikevee <odahviktor@gmail.com>
2023-06-23 10:24:05 +0100

    sa

--- script.js
+++ script.js
@@ -310,3 +310,41 @@ window.addEventListener('load', function() {
     element.style.display = 'block';
   }
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+   accordionItemHeader.addEventListener("click", event => {
+    
+    //  Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+    const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+       currentlyActiveAccordionItemHeader.classList.toggle("active");
+       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+     }
+
+     accordionItemHeader.classList.toggle("active");
+     const accordionItemBody = accordionItemHeader.nextElementSibling;
+     if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+     }
+     else {
+       accordionItemBody.style.maxHeight = 0;
+     }
+    
+   });
+});
